ea9c3ace9f1efa856c4a186d89d6019cdc2400f4
Alber <Alber@LAPTOP-VRJ6TSF7>
2019-11-13 08:31:45 +0000

    Start the project

---
+++ src/kata5/CurrencyDialog.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata5;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Alber
+ */
+public class CurrencyDialog extends JPanel {
+    
+     public CurrencyDialog(){
+        super();
+        add(createCombobox());
+    }
+
+    private JComboBox createCombobox() {
+        return new JComboBox(new String[] {"EUR","USD","GBP"});
+    }
+}
