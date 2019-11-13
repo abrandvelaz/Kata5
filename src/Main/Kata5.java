4e6d51c0b88f01f5191be9082285ba0b99a08a81
Alber <Alber@LAPTOP-VRJ6TSF7>
2019-11-13 09:59:54 +0000

    Start kata5 version 1

---
+++ src/Main/Kata5.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class Kata5 {
+
+    public static void main(String[] args) {
+        String URL_BD_SQLite = new String("jdbc:sqlite:C:\\Users\\Alber\\Documents\\NetBeansProjects\\DB_Sql\\tabla.db");
+        Connection connection = connect(URL_BD_SQLite);
+    }
+
+    private static Connection connect(String URL_BD_SQLite) {
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection(URL_BD_SQLite);
+            System.out.println("Base de datos conectada...");
+            selectData_PEOPLE(connection);
+        }
+        catch(SQLException exception){
+            System.out.println("Error Kata5 ::Connect " + exception.getMessage());
+        }
+        finally{
+            try{
+                if(connection != null){
+                    connection.close();
+                }
+            }
+            catch(SQLException exception){
+                System.out.println("Error Kata5 ::Connect " + exception.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    private static void selectData_PEOPLE(Connection connection) {
+        String SQL = "SELECT * FROM PEOPLE";
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultset = statement.executeQuery(SQL);
+            System.out.println("ID \t NOMBRE \t APELLIDOS \t DEPARTAMENTO");
+            while(resultset.next()){
+                System.out.println(resultset.getInt("ID") + " \t " + 
+                                   resultset.getString("NOMBRE") + " \t \t " + 
+                                   resultset.getString("APELLIDOS") + " \t " + 
+                                   resultset.getString("DEPARTAMENTO"));
+            }
+        }
+        catch(SQLException exception){
+            System.out.println("Error Kata5 ::selectData_PEOPLE " + exception.getMessage());
+        }
+    }
+}
