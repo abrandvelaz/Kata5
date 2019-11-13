7ae1ab29d092eb46e511145beedda7dc19d1dc90
Alber <Alber@LAPTOP-VRJ6TSF7>
2019-11-13 09:31:07 +0000

    Create the MailListreader

---
+++ src/kata5/MailListreader.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alber
+ */
+public class MailListreader {
+    
+     public static List<String> read(String fileName) throws Exception {
+        List<String> lista = new ArrayList<>();
+        File file = new File(fileName);
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String email = sc.nextLine();
+                if (email.indexOf('@') != -1) {
+                    lista.add(email);
+                }
+            }
+            sc.close();
+        } 
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+     
+}
